bb7dff08c641dc6214a8fbb6889546c8c65c949f
Anchal Chaudhary <anchal.ldn5@gmail.com>
2016-11-01 02:02:49 +0530

    Registration, AddSkills

---
+++ OnlineBarterSystemP1/App_Code/SqlConnectionClass.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
+
+/// <summary>
+/// Summary description for UserDetails
+/// </summary>
+namespace BarterSystem
+{
+    public class SqlConnectionClass
+    {
+        SqlConnection connect = new SqlConnection();
+        public SqlConnectionClass()
+        {
+            connect.ConnectionString= ConfigurationManager.ConnectionStrings["UserDetailsConnectionString"].ConnectionString;
+        }
+        public void Add_Details_to_table(string command)
+        {
+            using (SqlCommand cmd = new SqlCommand(command, connect))
+            {
+                connect.Open();
+                cmd.ExecuteNonQuery();
+            }
+        }
+    }
+}
---
+++ OnlineBarterSystemP1/App_Code/User.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+/// <summary>
+/// Summary description for User
+/// </summary>
+namespace BarterSystem
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Gender { get; set; }
+        public string City { get; set; }
+        public string Phone { get; set; }
+        public string Dob { get; set; }
+        //public byte[] ProfilePicture { get; set; }
+       
+
+        #region Signup function
+        public void SignUp(User objUser)
+        {
+            int emailCount = 0;
+            string connString = ConfigurationManager.ConnectionStrings["UserDetailsConnectionString"].ConnectionString;
+            using (SqlConnection connect = new SqlConnection(connString))
+            {
+                using (SqlCommand cmdEmail = new SqlCommand("select count(*) from tblUser where Email='" +objUser.Email + "'"))
+                {
+                    cmdEmail.Connection = connect;
+                    connect.Open();
+                    emailCount = Convert.ToInt32(cmdEmail.ExecuteScalar().ToString());
+                }
+            }
+            using (SqlConnection connect1 = new SqlConnection(connString))
+            {
+                if (emailCount == 0)
+                {
+                    using (SqlCommand cmdDetails = new SqlCommand("insert into tblUser(Name, Email, Password, Gender, City, Phone, Dob) values (@Name, @Email, @Password, @Gender, @City, @Phone, @Dob)"))
+                    {
+                        cmdDetails.Connection = connect1;
+                        connect1.Open();
+                        cmdDetails.Parameters.AddWithValue("@Name", objUser.Name);
+                        cmdDetails.Parameters.AddWithValue("@Email", objUser.Email);
+                        cmdDetails.Parameters.AddWithValue("@Password", objUser.Password);
+                        cmdDetails.Parameters.AddWithValue("@Gender", objUser.Gender);
+                        cmdDetails.Parameters.AddWithValue("@City", objUser.City);
+                        cmdDetails.Parameters.AddWithValue("@Phone", objUser.Phone);
+                        cmdDetails.Parameters.AddWithValue("@Dob", objUser.Dob);
+                        //connect1.Open();
+                        cmdDetails.ExecuteNonQuery();
+                    }
+                }
+            }
+        }
+        #endregion
+    }
+}
