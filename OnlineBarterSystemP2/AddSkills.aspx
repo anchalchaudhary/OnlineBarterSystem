f9b6de70e6466c07df45ef94b753ada85f5b5f63
Anchal Chaudhary <anchal.ldn5@gmail.com>
2016-11-04 14:20:40 +0530

    Validations, Profile Picture, login

---
+++ OnlineBarterSystemP2/AddSkills.aspx
@@ -0,0 +1,30 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="AddSkills.aspx.cs" Inherits="AddSkills" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>ADD YOUR SKILLS</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <p>
+            <asp:Label ID="lblSelectGenre" runat="server" Text="Genre Name"></asp:Label>
+            <asp:DropDownList ID="ddlGenre" runat="server" AutoPostBack="true" OnSelectedIndexChanged="ddlGenre_SelectedIndexChange"></asp:DropDownList>
+        </p>
+        <p>
+            <asp:Label ID="lblEnterGenre" runat="server" Text="Enter Name of the Genre"></asp:Label>
+            <asp:TextBox ID="tbxGenreName" runat="server" input type="Text"></asp:TextBox>
+        </p>
+        <p>
+            <asp:Label ID="lblEnterSkill" runat="server" Text="Enter Your Skill"></asp:Label>
+            <asp:TextBox ID="tbxEnterSkill" runat="server"></asp:TextBox>
+        </p>
+        <p>
+            <asp:Button ID="btnSubmit" runat="server" Text="Submit" OnClick="btnSubmit_click" />
+        </p>
+    </div>
+    </form>
+</body>
+</html>
---
+++ OnlineBarterSystemP2/AddSkills.aspx.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+public partial class AddSkills : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        tbxGenreName.Enabled = false;
+        if (!this.IsPostBack)
+        {
+            string connString = ConfigurationManager.ConnectionStrings["UserDetailsConnectionString"].ConnectionString;
+            using (SqlConnection connect = new SqlConnection(connString))
+            {
+                using (SqlCommand cmd = new SqlCommand("SELECT Id, Genre FROM tblGenre"))
+                {
+                    cmd.Connection = connect;
+                    connect.Open();
+                    ddlGenre.DataSource = cmd.ExecuteReader();
+                    ddlGenre.DataTextField = "Genre";
+                    ddlGenre.DataValueField = "Id";
+                    ddlGenre.DataBind();
+                    connect.Close();
+                }
+            }
+
+            ddlGenre.Items.Insert(0, new ListItem("Select", "-1"));
+            ddlGenre.Items.Insert(1, new ListItem("Other", "0"));
+        }
+    }
+    protected void ddlGenre_SelectedIndexChange(object sender, EventArgs e)
+    {
+        if (ddlGenre.SelectedItem.Text == "Other")
+            tbxGenreName.Enabled = true;
+        else
+            tbxGenreName.Enabled = false;
+    }
+
+    protected void btnSubmit_click(object sender, EventArgs e)
+    {
+        int skillCount = 0;
+        string connString = ConfigurationManager.ConnectionStrings["UserDetailsConnectionString"].ConnectionString;
+        using (SqlConnection connect = new SqlConnection(connString))
+        {
+            using (SqlCommand cmd = new SqlCommand("Select count(*) from tblSkills where SkillName='" + tbxEnterSkill.Text + "'"))
+            {
+                cmd.Connection = connect;
+                connect.Open();
+                skillCount = Convert.ToInt32(cmd.ExecuteScalar().ToString());
+            }
+        }
+        if (skillCount == 0) //to add skill to tblSkills if not present
+        {
+            using (SqlConnection connect = new SqlConnection(connString))
+            {
+                using (SqlCommand cmdAddSkill = new SqlCommand("INSERT INTO tblSkills (SkillName, GenreId) values (@SkillName, @GenreId)"))
+                {
+                    cmdAddSkill.Connection = connect;
+                    connect.Open();
+                    cmdAddSkill.Parameters.AddWithValue("@SkillName", tbxEnterSkill.Text);
+                    cmdAddSkill.Parameters.AddWithValue("@GenreId", ddlGenre.SelectedValue);
+                    cmdAddSkill.ExecuteNonQuery();
+                }
+            }
+        }
+        //using (SqlConnection connect = new SqlConnection(connString))
+        //{
+          //  using (SqlCommand cmdMapSkill = new SqlCommand("INSERT INTO tblSkills")
+    }
+}
