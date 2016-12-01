b829b9b4ef7320ac30ec03eadfd4a2b803b6d2fd
Anchal Chaudhary <anchal.ldn5@gmail.com>
2016-12-01 12:13:47 +0530

    Verification link Encrypted

---
+++ OnlineBarterSystemP15/User/ChangePassword.aspx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Configuration;
+
+public partial class User_ChangePassword : System.Web.UI.Page
+{
+    string USERID = "";
+    protected void Page_Load(object sender, EventArgs e)
+    {
+         USERID = Request.QueryString["UserId"];
+        
+    }
+    protected void Change(object sender,EventArgs e)
+    {
+        string encrytedPassword = Hash.Encrypt_Password(tbxPassword.Text);
+        string connString = ConfigurationManager.ConnectionStrings["UserDetailsConnectionString"].ConnectionString;
+        using (SqlConnection connect = new SqlConnection(connString))
+        {
+            using (SqlCommand cmd = new SqlCommand("update tblUsers set Password=@Password where Id='" + USERID + "'"))
+            {
+                cmd.Connection = connect;
+                cmd.Parameters.AddWithValue("@Password", encrytedPassword);
+                connect.Open();
+                cmd.ExecuteNonQuery();
+                connect.Close();
+            }
+        }
+        Response.Redirect("Login.aspx");
+    }
+}
---
+++ OnlineBarterSystemP15/User/Verification.aspx.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Text;
+using System.IO;
+
+public partial class User_Verification : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        int count;
+        string USERID = Request.QueryString["UserId"];
+        string connString = ConfigurationManager.ConnectionStrings["UserDetailsConnectionString"].ConnectionString;
+        using (SqlConnection connect = new SqlConnection(connString))
+        {
+            using (SqlCommand cmd = new SqlCommand("select count(*) from tblUsers where Id='" + USERID + "'"))
+            {
+                cmd.Connection = connect;
+                connect.Open();
+                count = Convert.ToInt32(cmd.ExecuteScalar());
+            }
+        }
+
+        if (count != 0)
+        {
+            using (SqlConnection connect = new SqlConnection(connString))
+            {
+                using (SqlCommand cmd = new SqlCommand("Update tblUsers set Activated=1 where Id=@Id"))
+                {
+                    cmd.Connection = connect;
+                    connect.Open();
+                    cmd.Parameters.AddWithValue("@Id", USERID);
+                    cmd.ExecuteNonQuery();
+                    Response.Redirect("Login.aspx");
+                }
+            }
+        }
+    }
+}
